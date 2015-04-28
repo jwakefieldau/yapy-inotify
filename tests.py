8522e66c833253b864b847f6f3c80f5bf897966b
James <james@james-laptop.(none)>
2015-04-29 07:15:59 +1000

    * rough skeleton of unit testing

--- tests.py
+++ tests.py
@@ -1,3 +1,35 @@
-from unittest import TestSuite, TestCase
+from unittest import TestCase
 
-#TODO
+# Test cases:
+
+# creation of file in directory
+# reading of file
+# modification of file
+# closing of file
+# deletion of file
+
+# recursive dir watch
+# creation of files in multiple directories in tree
+# 
+
+# opening of existing file
+# 
+
+# *** WHICH TESTS TO RUN FOR RECURSIVE DIR WATCH VS SINGLE FILE/DIR ? ***
+# if some tests have been done for single case, do they need to be re-done for recursive?
+
+"""
+class InotifyTestCase(TestCase):
+
+	def _test_file_name(self):
+		# return a test file name that should be collision resistant,
+		# maybe test_file_root_dir/test_file.PID.UUID4 ?
+
+	def setUp(self):
+		# make test file root dir
+		# create test file name
+		# create eventdispatcher
+
+	## specific tests
+"""		
+
