c329ea986d11d99a0f2a4d863c0ea5fa3803e51f
James <james@james-laptop.(none)>
2015-09-04 16:46:08 +1000

    * add requirement for install - Cython

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
+	# how does name= affect things?
 	name='inotify',
+	install_requires='Cython>=0.20.2'
 	ext_modules=cythonize([Extension('inotify', ['inotify.pyx'])]),
 )
